fafef76da47ec2086ceba282a98457e5de6d71ff
Shivansh Pareek <65125848+shivzz-creator@users.noreply.github.com>
2022-05-29 14:37:41 +0530

    Update scorecard.js

--- scorecard.js
+++ scorecard.js
@@ -1,131 +1,122 @@
-// const url =
-//   "https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
-
-const request = require("request");
-const cheerio = require("cheerio");
+const request = require('request')
+const cheerio = require('cheerio')
 const path = require("path");
 const fs = require("fs");
 
 const xlsx = require('xlsx')
 
-function processScoreCrad(url) {
-  request(url, cb);
+function processScoreCard(url) {
+    request(url, cb)
 }
+
 
 function cb(err, response, html) {
-  if (err) {
-    console.error(err);
-  } else {
-    extractMatchDetails(html);
-  }
+    if (err) {
+        console.log(err)
+    }
+    else {
+        extractScoreCardHtml(html)
+    }
 }
 
-function extractMatchDetails(html) {
-  let $ = cheerio.load(html);
+let venue
+let date
+let result
+function extractScoreCardHtml(html) {
+    let $ = cheerio.load(html)
+    let descElem = $('[class="ds-text-tight-m ds-font-regular ds-text-ui-typo-mid"]').text()
+    // console.log(descElem)
+    let descArr = descElem.split(',')
+    //jab bhi split func use krna h to trim() use krna h to remove extra spaces
 
-  let descString = $(".match-header-info.match-info-MATCH .description");
+    venue = descArr[1].trim()
+    date = descArr[2].trim()
 
-  let descStringArr = descString.text().split(",");
+    result = $('.ds-text-tight-m.ds-font-regular.ds-truncate.ds-text-typo-title').text()
+    // console.log(descArr)
 
-  let venue = descStringArr[1].trim();
-  let date = descStringArr[2].trim();
+    // console.log(venue)
+    // console.log(date)
+    // console.log(result)
 
-  let result = $(
-    ".match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span"
-  ).text();
+    getTableHtml(html)
 
-  console.log(venue);
-  console.log(date);
-  console.log(result);
+}
 
-  console.log("```````````````````````````````````````````````````");
+function getTableHtml(html) {
+    let $ = cheerio.load(html)
+    let tableArr = $('[class=" ds-mb-4"]')
 
-  let innings = $(".card.content-block.match-scorecard-table>.Collapsible");
+    let tableHtml = ""
+    for (let i = 0; i < tableArr.length; i++) {
 
-  //console.log(innings)
+     
+        tableHtml += $(tableArr[i]).html()
 
-  let htmlString = "";
+       
+        // console.log(tableArr[i])
+        let teamName = $(tableArr[i]).find('.ds-grow').text()
+     
+        teamName = teamName.split('INNINGS')[0]
+        let opponentIndex = (i == 0) ? 1 : 0
 
-  for (let i = 0; i < innings.length; i++) {
-    htmlString += $(innings[i]).html();
+        let opponentName = $(tableArr[opponentIndex]).find('.ds-py-3').text()
+        opponentName = opponentName.split('INNINGS')[0]
 
-    let teamName = $(innings[i]).find("h5").text();
-    teamName = teamName.split("INNINGS")[0].trim();
-    let opponentIndex = i == 0 ? 1 : 0;
+        // console.log(teamName)
+        // console.log(teamName, opponentName)
 
-    let opponentName = $(innings[opponentIndex]).find("h5").text();
-    opponentName = opponentName.split("INNINGS")[0].trim();
+        let cInning = $(tableArr[i])
+        let allRows = cInning.find('[class="ds-w-full ds-table ds-table-xs ds-table-fixed ci-scorecard-table"] tr')
 
-    //console.log(teamName , opponentName);
+        for (let j = 0; j < allRows.length; j++) {
+           
+            let allCols = $(allRows[j]).find('td')
 
-    let cInning = $(innings[i]);
+            let isWorthy = $(allCols).length>4
+                        
+            if (isWorthy) {
+                //console.log(allCols[0])
+                let playerName = $(allCols[0]).text().trim();
+                let runs = $(allCols[2]).text().trim();
+                let balls = $(allCols[3]).text().trim();
+                let fours = $(allCols[5]).text().trim();
+                let sixes = $(allCols[6]).text().trim();
+                let STR = $(allCols[7]).text().trim();
 
-    let allRows = cInning.find(".table.batsman tbody tr");
+                console.log(
+                    `${playerName} | ${runs} | ${balls} | ${fours} | ${sixes} | ${STR}`
+                );
 
-    for (let j = 0; j < allRows.length; j++) {
-      let allCols = $(allRows[j]).find("td");
-      let isWorthy = $(allCols[0]).hasClass("batsman-cell");
+                processPlayer(
+                    teamName,
+                    opponentName,
+                    playerName,
+                    runs,
+                    balls,
+                    fours,
+                    sixes,
+                    STR,
+                    venue,
+                    date,
+                    result
+                );
+            }
 
-      if (isWorthy == true) {
-        let playerName = $(allCols[0]).text().trim();
 
-        let runs = $(allCols[2]).text().trim();
-        let balls = $(allCols[3]).text().trim();
-        let fours = $(allCols[5]).text().trim();
-        let sixes = $(allCols[6]).text().trim();
-        let STR = $(allCols[7]).text().trim();
+        }
 
-        console.log(
-          `${playerName} | ${runs} |${balls} | ${fours} | ${sixes} | ${STR}`
-        );
-
-        processPlayer(
-          teamName,
-          opponentName,
-          playerName,
-          runs,
-          balls,
-          fours,
-          sixes,
-          STR,
-          venue,
-          date,
-          result
-        );
-        // Template Literal
-      }
+        // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     }
 
-    console.log("````````````````````````````````````````````````````````");
-  }
 
-  //console.log(htmlString)
+    // console.log(tableHtml)
 }
 
 function processPlayer(
-  teamName,
-  opponentName,
-  playerName,
-  runs,
-  balls,
-  fours,
-  sixes,
-  STR,
-  venue,
-  date,
-  result
-) {
-  let teamPath = path.join(__dirname, "IPL", teamName);
-  dirCreator(teamPath);
-
-  let filePath = path.join(teamPath, playerName + ".xlsx");
-
-  let content = excelReader(filePath, playerName); []
-
-  let playerObj = {
-    playerName,
     teamName,
     opponentName,
+    playerName,
     runs,
     balls,
     fours,
@@ -133,40 +124,64 @@ function processPlayer(
     STR,
     venue,
     date,
-    result,
-  };
+    result
+) {
+    let teamPath = path.join(__dirname, "IPL", teamName);
+    // console.log(teamPath)
+    dirCreator(teamPath);
 
-  content.push(playerObj)
+    let filePath = path.join(teamPath, playerName+ ".xlsx");
 
-  excelWriter(filePath , playerName , content )
+    let content = excelReader(filePath, playerName);[]
+
+    
+    let playerObj = {
+        playerName,
+        teamName,
+        opponentName,
+        runs,
+        balls,
+        fours,
+        sixes,
+        STR,
+        venue,
+        date,
+        result,
+    };
+
+    content.push(playerObj)
+
+    excelWriter(filePath, playerName, content)
 }
 
 function dirCreator(folderPath) {
-  if (fs.existsSync(folderPath) == false) {
-    fs.mkdirSync(folderPath);
-  }
+    if (fs.existsSync(folderPath) == false) {
+        fs.mkdirSync(folderPath);
+    }
 }
 
 function excelWriter(fileName, sheetName, jsonData) {
-  let newWB = xlsx.utils.book_new();
-  // Creating a new WorkBook
-  let newWS = xlsx.utils.json_to_sheet(jsonData);
-  // Json is converted to sheet format (rows and cols)
-  xlsx.utils.book_append_sheet(newWB, newWS, sheetName);
-  xlsx.writeFile(newWB, fileName);
+    let newWB = xlsx.utils.book_new();
+    // Creating a new WorkBook
+    let newWS = xlsx.utils.json_to_sheet(jsonData);
+    // Json is converted to sheet format (rows and cols)
+    xlsx.utils.book_append_sheet(newWB, newWS, sheetName);
+    xlsx.writeFile(newWB, fileName);
 }
 
 function excelReader(fileName, sheetName) {
-  if (fs.existsSync(fileName) == false) {
-    return [];
-  }
-  let wb = xlsx.readFile(fileName);
+    if (fs.existsSync(fileName) == false) {
+        return [];
+    }
+    let wb = xlsx.readFile(fileName);
 
-  let excelData = wb.Sheets[sheetName];
-  let ans = xlsx.utils.sheet_to_json(excelData);
-  return ans
+    let excelData = wb.Sheets[sheetName];
+    let ans = xlsx.utils.sheet_to_json(excelData);
+    return ans
 }
 
+
+
 module.exports = {
-  ps: processScoreCrad,
-};
+    ps: processScoreCard
+}
